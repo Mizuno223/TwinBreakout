4916f72c06b1c312c9c273da7d9e992610993acc
Yuto Mizuno <31834@toyota.kosen-ac.jp>
2022-02-06 01:40:02 +0900

    Add exit function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,5 +14,13 @@ public class Player : MonoBehaviour
     void Update()
     {
         myRigidbody.velocity = new Vector3 (Input.GetAxis("Horizontal") * speed, 0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;   // UnityEditorの実行を停止する処理
+    #else
+            Application.Quit();                                // ゲームを終了する処理
+    #endif
+        }
     }
 }
